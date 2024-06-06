4c94ec0fa513d672187fa3ba026e7d73979238ef
staticshock7 <murra275@purdue.edu>
2024-06-06 10:35:46 -0400

    Update users.ps1

--- users.ps1
+++ users.ps1
@@ -1,9 +1,9 @@
-# NOTE: no need to test connection for OUs, and LastLoggedOnUser
+# NOTE: no need to test connection for LastLoggedOnUser
 
-# Path to the file containing computer names
+# file containing computer names
 $computerNamesFile = "C:\temp\banana.txt"
 
-# Check if the file exists
+# if the file exists
 if (-not (Test-Path $computerNamesFile)) {
     Write-Host "Serial Number of ${computerName}: ${serialNumber}"
     exit
@@ -17,20 +17,21 @@ foreach ($computerName in $computerNames) {
     # Check if the computer is reachable
     if (Test-Connection -ComputerName $computerName -Count 1 -Quiet) {
 
-	<#
+	
 	
 	# Gets the OUs for the PCs in the list
 	$ou = Add-Content -Path ".\OUs.txt" ((Get-EMComputerDetails $computerName | find "ECN Computers"))
+ 	# Gets the Service Tags for the PCs in the list
 	$serialNumber = Add-Content -Path ".\OUs.txt" ((Get-WmiObject Win32_BIOS -ComputerName $computerName).SerialNumber)
 
-	# If the PC has VISUAL STUDIO
+	# If the PC has VISUAL STUDIO (Office 2021, I think)
 	$visio = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" }
-	# OR
-	$visio = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Office\root\Office16" }
+	# OR has VISUAL STUDIO (Office 365, I think)
+	$visio = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Office\root\Office16\VISIO.exe" }
         Add-Content -Path ".\VISIO.txt" -Value "${visio}"
 
-	# If the PC has MSProject (for Office 2019 install)
-	$project = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Office\root\Office16\WINPROJ.exe" } #VISIO Office 365 I think
+	# If the PC has MSProject (for Office 2019 install, I think)
+	$project = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Office\root\Office16\WINPROJ.exe" } 
 	Add-Content -Path ".\MSproject.txt" -Value "${MSproject}"
 
 	
@@ -41,7 +42,7 @@ foreach ($computerName in $computerNames) {
 	$line2 = ($line2 -split ' ')[0]	
 	Add-Content -Path ".\userss.txt" (($line2))
 
-	#>
+	
 
 	$visio = Invoke-Command -ComputerName $computerName -ScriptBlock { Test-Path "C:\Program Files\Microsoft Office\root\Office16\VISIO.exe" } # VISIO Office 2019 I think
         Add-Content -Path ".\VISIO.txt" -Value "${visio}"
@@ -49,6 +50,7 @@ foreach ($computerName in $computerNames) {
     } else {
 
 	# used to place gaps for PCs that cannot be pinged
+ 	# uncomment the one's being used I guess
 
         #Add-Content -Path ".\OUs.txt" -Value "NULL"
         #Add-Content -Path ".\SERIALs.txt" -Value "NULL"
